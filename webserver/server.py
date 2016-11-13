e425adc8560b7fc0723658e66cef0a2c63afa622
yx2387 <yx2387@columbia.edu>
2016-11-13 21:07:54 +0000

    updated instructor

--- webserver/server.py
+++ webserver/server.py
@@ -188,38 +188,38 @@ def index():
 # the functions for each app.route needs to have different names
 #
 
-@app.route('/get_course/<cid>/<csession>')
-def get_course(cid,csession):
+@app.route('/get_course/<cid>/<csession>/<cyear>/<cseason>')
+def get_course(cid,csession,cyear,cseason):
   print cid,csession
   cursor = g.conn.execute(
   """select c.course_id,c.session,c.course_name,o.year,o.season,u.user_name,c.credit,o.time,o.location,c.syllabus \
   from course c, open o,  instruct i,users u \
   where u.user_id=i.user_id and i.open_cid = o.open_cid 
-  and o.course_id = %s and o.session = %s and c.course_id = o.course_id and o.session = c.session"""
-  , cid,csession)
+  and o.course_id = %s and o.session = %s and c.course_id = o.course_id and o.session = c.session and o.year = %s and o.season = %s"""
+  , cid,csession,cyear,cseason)
   Courses = [dict(id=row[0], session=row[1], name=row[2], year=row[3], season=row[4], inst=row[5], 
   credit=row[6], time=row[7], location=row[8], syllabus=row[9]) for row in cursor.fetchall()]
   cursor.close()
-  print Courses
+#  print Courses
 
   cursor = g.conn.execute(
   """select a.assign_id, a.assign_title, a.assign_description, a.points, a.due_date, p.time
   from postassignment p, assignment a, open o, course c
   where o.course_id = %s and o.session = %s and c.course_id = o.course_id and o.session = c.session and p.open_cid = o.open_cid
-  and p.assign_id = a.assign_id"""
-  ,cid , csession)
+  and p.assign_id = a.assign_id and o.year = %s and o.season = %s"""
+  ,cid , csession,cyear,cseason)
   Assigns = [dict(id=row[0], title=row[1], des=row[2], points=row[3], due=row[4], time=row[5]) for row in cursor.fetchall()]
-  print Assigns
+#  print Assigns
   cursor.close()
   
   cursor = g.conn.execute(
   """select a.ann_id, a.ann_title, a.ann_content, p.time
   from postannouncement p, announcement a, open o, course c
   where o.course_id = %s and o.session = %s and c.course_id = o.course_id and o.session = c.session and p.open_cid = o.open_cid
-  and p.ann_id = a.ann_id"""
-  ,cid , csession)
+  and p.ann_id = a.ann_id and o.year = %s and o.season = %s"""
+  ,cid , csession,cyear,cseason)
   Anns = [dict(id=row[0], title=row[1], des=row[2], time=row[3]) for row in cursor.fetchall()]
-  print Anns
+#  print Anns
   cursor.close()
 
   return render_template("course.html",Courses=Courses,Assigns = Assigns,Anns=Anns)
@@ -230,7 +230,66 @@ def another():
 
 @app.route('/home_i')
 def home_i():
-  return render_template("instructor.html")
+  if 'i' in session:
+    print session['i']
+    cursor = g.conn.execute(
+    """select c.course_id,c.session,c.course_name,o.year,o.season,u.user_name,o.open_cid 
+    from course c, open o, instruct i,users u 
+    where u.user_id=i.user_id and i.open_cid = o.open_cid and i.user_id = %s 
+    and o.course_id = c.course_id and o.session = c.session"""
+    , session['i'])
+    Courses = [dict(id=row[0], session=row[1], name=row[2], year=row[3], season=row[4], inst=row[5],oid=row[6]) for row in cursor.fetchall()]
+    cursor.close()
+
+    cursor = g.conn.execute(
+    """select distinct u.user_id, u.user_name, u.gender, d.dep_name, i.title_level, u.email, u.phone
+    from users u, belongs b, department d, instructor i
+    where u.user_id = %s and u.user_id = i.user_id  
+    """
+    ,session['i'])
+    Userinfo = [dict(id=row[0], name=row[1], gender=row[2], dep=row[3], title=row[4], email=row[5], phone=row[6]) for row in cursor.fetchall()]
+    cursor.close()
+
+    print Userinfo
+#    print Courses
+
+    cursor = g.conn.execute(
+    """select q.que_title, q.que_description, u.user_name, a.time, q.que_id
+    from question q, ask a, users u
+    where u.user_id = a.user_id and q.que_id = a.que_id
+    """
+    )
+    Ques = [dict(title=row[0], des=row[1], name=row[2], time=row[3], id=row[4]) for row in cursor.fetchall()]
+#    print Ques
+    cursor.close()
+
+
+    cursor = g.conn.execute(
+    """select r.ans_content, u2.user_name, r.time, q.que_id
+    from question q, reply r, users u1, users u2, ask a
+    where u1.user_id = a.user_id and q.que_id = a.que_id and r.que_id = q.que_id and r.user_id = u2.user_id
+    """
+    )
+    Ans = [dict(content=row[0], name=row[1], time=row[2], id=row[3]) for row in cursor.fetchall()]
+#    print Ans
+    cursor.close()
+
+
+    cursor = g.conn.execute(
+    """select a.assign_id, a.assign_title, a.assign_description, a.points, a.due_date, p.time, c.course_id, c.course_name, o.open_cid
+    from postassignment p, assignment a, open o, course c, instruct i, semester s
+    where i.user_id = %s and c.course_id = o.course_id and o.session = c.session and p.open_cid = o.open_cid
+    and p.assign_id = a.assign_id and i.open_cid = o.open_cid and s.year = o.year and s.season = o.season"""
+    ,session['i']) 
+    Assigns = [dict(id=row[0], title=row[1], des=row[2], points=row[3], due=row[4], time=row[5], cid=row[6], cname=row[7],oid=row[8]) for row in cursor.fetchall()]
+    print Assigns
+    cursor.close()
+
+
+    return render_template("instructor.html",Courses=Courses,Userinfo=Userinfo, Ques = Ques,Ans=Ans, Assigns = Assigns)
+  else:
+    return redirect('/')
+
 
 @app.route('/home_s')
 def home_s():
@@ -278,7 +337,21 @@ def home_s():
     cursor.close()
 
 
-    return render_template("student.html",Courses=Courses,Userinfo=Userinfo, Ques = Ques,Ans=Ans)
+    cursor = g.conn.execute(
+    """select distinct a.assign_title, a.due_date, c.course_id, c.session, c.course_name, o.open_cid
+    from users u, assignment a, open o, postassignment p, take t, course c, semester s
+    where t.user_id = %s and t.open_cid = o.open_cid and o.course_id = c.course_id and o.session = c.session
+    and o.open_cid = p.open_cid and p.assign_id = a.assign_id and a.due_date > CURRENT_TIMESTAMP - INTERVAL '37 days' 
+    and s.year = o.year and s.season = o.season
+    """
+    ,session['s'])
+#and a.due_date > CURRENT_TIMESTAMP - INTERVAL '35 days'
+    Assigns = [dict(title=row[0], due=row[1], id=row[2], session=row[3], name=row[4], oid=row[5]) for row in cursor.fetchall()]
+    print Assigns
+    cursor.close()
+
+
+    return render_template("student.html",Courses=Courses,Userinfo=Userinfo, Ques = Ques,Ans=Ans, Assigns = Assigns)
   else:
     return redirect('/')
 
