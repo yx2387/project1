078db49dbc8ead5d388cf8e07092c615a6736a2d
Fei Peng <fp2360@columbia.edu>
2016-11-14 11:55:00 -0500

    test modal

--- webserver/static/js/Course.js
+++ webserver/static/js/Course.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
-	$("#qa_ask").click(function(){
-		$("#showText").text("Answer Successfully!");
-		$("#modalShow").modal();
+	$("form#ask_form").submit(function() {
+		// var formData = new FormData($(this)[0]);
+
+	 //    $.post("/upload", formData, function(data) {
+		//     $("#showText").text("Answer Successfully!");
+		// 	$("#modalShow").modal();
+	 //    });
+	 $("#showText").text("Success!");
+	$("#modalShow").modal();
+	});
+
+	$("#modalShow").on('hide.bs.modal', function () {
+		var current_index = $("#tabs").tabs("option","active");
+		$("#tabs").tabs('load',current_index);
 	});
 });
