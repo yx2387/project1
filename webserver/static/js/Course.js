05d565acf98e7c4a018a0cd8b5dabc621764a769
Fei Peng <fp2360@columbia.edu>
2016-11-14 16:13:54 -0500

    ask ajax

--- webserver/static/js/Course.js
+++ webserver/static/js/Course.js
@@ -1,19 +1,20 @@
 $(document).ready(function(){
 	$("#ask_btn").click(function() {
-		var url = "/ask/" + $("#ask_btn").attr("uid") + "/" + $("#ask_btn").attr("oid");
-	    $.post(url,
-		{
-		    title:$("#title").val(),
-		    des:$("#des").val()
-		},
-		function(data){
-		    $("#showText").text(data);
+	// 	var url = "/ask/" + $("#ask_btn").attr("uid") + "/" + $("#ask_btn").attr("oid");
+	//     $.post(url,
+	// 	{
+	// 	    title:$("#title").val(),
+	// 	    des:$("#des").val()
+	// 	},
+	// 	function(data){
+	// 	    $("#showText").text(data);
+	// 		$("#modalShow").modal();
+	// 	});
+	$("#showText").text("Success");
 			$("#modalShow").modal();
-		});
 	});
 
 	$("#modalShow").on('hide.bs.modal', function () {
-		var current_index = $("#tabs").tabs("option","active");
-		$("#tabs").tabs('load',current_index);
+		location.reload();
 	});
 });
