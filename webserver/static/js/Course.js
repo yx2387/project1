9ec592d73c9e4c0c57bfa2988461c8cfd79b34d7
Fei Peng <fp2360@columbia.edu>
2016-11-15 10:07:43 -0500

    fix reload tab issues

--- webserver/static/js/Course.js
+++ webserver/static/js/Course.js
@@ -1,21 +1,34 @@
 $(document).ready(function(){
 	$("#ask_btn").click(function() {
-		// var url = "/ask/" + $("#ask_btn").attr("uid") + "/" + $("#ask_btn").attr("oid");
-	 //    $.post(url,
-		// {
-		//     title:$("#title").val(),
-		//     des:$("#des").val()
-		// },
-		// function(data){
-		//     $("#showText").text(data);
-		// 	$("#modalShow").modal();
-		// });
-		$("#showText").text("111");
+		var url = "/ask/" + $("#ask_btn").attr("uid") + "/" + $("#ask_btn").attr("oid");
+	    $.post(url,
+		{
+		    title:$("#title").val(),
+		    des:$("#des").val()
+		},
+		function(data){
+		    $("#showText").text(data);
 			$("#modalShow").modal();
+		});
 	});
 
 	$("#modalShow").on('hide.bs.modal', function () {
 		location.reload();
-		$('.nav-tabs a[href="#' + "qa_panel" + '"]').tab('show');
 	});
+
+	$('ul.navbar-nav li a').click(function(){
+        var a_anchor = $(this).attr('href');
+        var ul_id = $(this).parent().parent().attr('id');
+        //该nav tabs下的current设置
+        localStorage.setItem(ul_id, a_anchor);
+    });
+
+    $('ul.navbar-nav').each(function(){
+        var tab_list = $(this);
+        var current = localStorage.getItem(tab_list.attr('id'));
+        if (current) {
+            var link = tab_list.find('a[href="'+current+'"]');
+            link.tab('show');
+        }
+    });
 });
--- webserver/static/js/Instructor.js
+++ webserver/static/js/Instructor.js
@@ -1,14 +1,30 @@
 $(document).ready(function(){
+	$('ul.navbar-nav li a').click(function(){
+        var a_anchor = $(this).attr('href');
+        var ul_id = $(this).parent().parent().attr('id');
+        //该nav tabs下的current设置
+        localStorage.setItem(ul_id, a_anchor);
+    });
+
+    $('ul.navbar-nav').each(function(){
+        var tab_list = $(this);
+        var current = localStorage.getItem(tab_list.attr('id'));
+        if (current) {
+            var link = tab_list.find('a[href="'+current+'"]');
+            link.tab('show');
+        }
+    });
+
 	$('#datetimepicker').datetimepicker();
 
 	$("#logout").click(function(){
 		window.location.href = "/logout_i";
 	});
 
-	$("#file_form").submit(function(e) {
-		e.preventDefault();
+	$("#file_upload_btn").click(function() {
+		alert("2345");
 
-	    var data = new FormData($(this)[0]);
+	    var data = new FormData($("#file_form")[0]);
 
 	    $.ajax({
 	        url: '/upload',
@@ -19,18 +35,19 @@ $(document).ready(function(){
 	            alert( data );
 	        }
 	    });
-
-  
 	});
 
 	$("#assfile_form").submit(function(event) {
-		// event.preventDefault();
-		// var formData = new FormData($(this)[0]);
+		alert("222");
+		event.preventDefault();
+		var formData = new FormData($(this)[0]);
 
 	    $.post($("#assfile_form").attr("action"), formData, function(data) {
+	    	alert("enter");
 		    $("#showText").text(data);
 			$("#modalShow").modal();
 	    });
+	    alert("end");
 	});
 
 	$("#ann_form").submit(function() {
@@ -52,7 +69,7 @@ $(document).ready(function(){
 	});
 
 
-	// $("#modalShow").on('hide.bs.modal', function () {
-	// 	location.reload();
-	// });
+	$("#modalShow").on('hide.bs.modal', function () {
+		location.reload();
+	});
 });
