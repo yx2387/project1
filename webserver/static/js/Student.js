ecdcc45537d33aa780af20f638cf3182fe7909fb
yx2387 <yx2387@columbia.edu>
2016-11-15 19:52:13 +0000

    merge

--- webserver/static/js/Student.js
+++ webserver/static/js/Student.js
@@ -3,4 +3,20 @@ $(document).ready(function(){
 		window.location.href = "/logout_s";
 	});
 
+	$('ul.navbar-nav li a').click(function(){
+        var a_anchor = $(this).attr('href');
+        var ul_id = $(this).parent().parent().attr('id');
+        //该nav tabs下的current设置
+        localStorage.setItem(ul_id, a_anchor);
+    });
+
+    $('ul.navbar-nav').each(function(){
+        var tab_list = $(this);
+        var current = localStorage.getItem(tab_list.attr('id'));
+        if (current) {
+            var link = tab_list.find('a[href="'+current+'"]');
+            link.tab('show');
+        }
+    });
+
 });
